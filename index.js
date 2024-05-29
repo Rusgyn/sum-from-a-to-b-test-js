8681aca8af62f0ba1e4efe964e7e10ef1524530b
rusgyn <rusgynmagsanay@gmail.com>
2024-05-29 17:29:39 -0400

    Implement the sum function using recursion.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let result = 0;
+  if (fromN === toN) {
+    return toN;
+  } else if (fromN < toN) {
+    result = result + (fromN + (sum((fromN + 1), toN)));
+  }
+  return result;
 }
 
 module.exports = sum;
+
